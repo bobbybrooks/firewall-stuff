fd80d576bd123fe5ce9c1e7066efa632e4dae895
Bobby Brooks <bobby@bobby-brooks.com>
2016-12-16 12:47:57 -0500

    Update block-china.sh

--- block-china.sh
+++ block-china.sh
@@ -6,6 +6,9 @@ rm cn.zone
 
 # Pull the latest IP set for China
 wget -P . http://www.ipdeny.com/ipblocks/data/countries/cn.zone
+# Add my own stuff
+echo "116.0.0.0/8" >> cn.zone
+sudo mv cn.zone /etc/cn.zone
 
 # Add each IP address from the downloaded list into the ipset 'china'
 for i in $(cat /etc/cn.zone ); do ipset -A china $i; done
